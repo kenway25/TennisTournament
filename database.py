aa565c54dd97569f0e15db03e86d1be6394f31dc
Prachet Makwana <88733493+kenway25@users.noreply.github.com>
2022-10-12 23:44:21 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# 
+# database.py -- implementation of a Swiss-system tournament
+# based on tournament.py from udacity.
+#
+
+import config as cfg
+import datetime
+import psycopg2
+import tools
+
+
+def connect():
+    # Connect to the PostgreSQL database.  Returns a database connection.
+    return psycopg2.connect(database=cfg.DATABASE_NAME,
+                            user=cfg.DATABASE_USERNAME,
+                            password=cfg.DATABASE_PASSWORD)
+
+
+def query(query):
+    connection = connect()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    if "SELECT" in query:
+        results = cursor.fetchall()
+    else:
+        results = ""
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return results
+
+
